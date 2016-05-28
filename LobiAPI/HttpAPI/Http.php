9d4ed82bc171694d458d07234e1e86b91c31db39
NewDelion <el.emena.crea@gmail.com>
2016-05-28 22:39:39 +0900

    なんか変だと思った

--- LobiAPI/HttpAPI/Http.php
+++ LobiAPI/HttpAPI/Http.php
@@ -18,20 +18,20 @@ class Http{
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		$req_header = [];
-		if($header.Host != '')
-			$req_header[] = 'Host: ' . $header.Host;
-		if($header.Connection)
+		if($header->Host != '')
+			$req_header[] = 'Host: ' . $header->Host;
+		if($header->Connection)
 			$req_header[] = 'Connection: keep-alive';
-		if($header.Accept != '')
-			$req_header[] = 'Accept: ' . $header.Accept;
-		if($header.UserAgent != '')
-			$req_header[] = 'User-Agent: ' . $header.UserAgent;
-		if($header.Referer != '')
-			$req_header[] = 'Referer: ' . $header.Referer;
-		if($header.AcceptEncoding != '')
-			$req_header[] = 'Accept-Encoding: ' . $header.AcceptEncoding;
-		if($header.AcceptLanguage != '')
-			$req_header[] = 'Accept-Language: ' . $header.AcceptLanguage;
+		if($header->Accept != '')
+			$req_header[] = 'Accept: ' . $header->Accept;
+		if($header->UserAgent != '')
+			$req_header[] = 'User-Agent: ' . $header->UserAgent;
+		if($header->Referer != '')
+			$req_header[] = 'Referer: ' . $header->Referer;
+		if($header->AcceptEncoding != '')
+			$req_header[] = 'Accept-Encoding: ' . $header->AcceptEncoding;
+		if($header->AcceptLanguage != '')
+			$req_header[] = 'Accept-Language: ' . $header->AcceptLanguage;
 		if(count($req_header) > 0)
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $req_header);
 		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_path);
@@ -47,22 +47,22 @@ class Http{
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		$req_header = [];
-		if($header.Host != '')
-			$req_header[] = 'Host: ' . $header.Host;
-		if($header.Connection)
+		if($header->Host != '')
+			$req_header[] = 'Host: ' . $header->Host;
+		if($header->Connection)
 			$req_header[] = 'Connection: keep-alive';
-		if($header.Accept != '')
-			$req_header[] = 'Accept: ' . $header.Accept;
-		if($header.Origin != '')
-			$req_header[] = 'Origin: ' . $header.Origin;
-		if($header.UserAgent != '')
-			$req_header[] = 'User-Agent: ' . $header.UserAgent;
-		if($header.Referer != '')
-			$req_header[] = 'Referer: ' . $header.Referer;
-		if($header.AcceptEncoding != '')
-			$req_header[] = 'Accept-Encoding: ' . $header.AcceptEncoding;
-		if($header.AcceptLanguage != '')
-			$req_header[] = 'Accept-Language: ' . $header.AcceptLanguage;
+		if($header->Accept != '')
+			$req_header[] = 'Accept: ' . $header->Accept;
+		if($header->Origin != '')
+			$req_header[] = 'Origin: ' . $header->Origin;
+		if($header->UserAgent != '')
+			$req_header[] = 'User-Agent: ' . $header->UserAgent;
+		if($header->Referer != '')
+			$req_header[] = 'Referer: ' . $header->Referer;
+		if($header->AcceptEncoding != '')
+			$req_header[] = 'Accept-Encoding: ' . $header->AcceptEncoding;
+		if($header->AcceptLanguage != '')
+			$req_header[] = 'Accept-Language: ' . $header->AcceptLanguage;
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $req_header);
 		curl_setopt($ch, CURLOPT_COOKIEFILE, $this->cookie_path);
 		curl_setopt($ch, CURLOPT_COOKIEJAR, $this->cookie_path);
--- LobiAPI/LobiAPI.php
+++ LobiAPI/LobiAPI.php
@@ -1,3 +1,25 @@
 <?php
 namespace delion\LobiAPI;
 
+use delion\LobiAPI\HttpAPI\Http;
+use delion\LobiAPI\HttpAPI\Header;
+
+class LobiAPI{
+	private $NetworkAPI = null;
+
+	public function __construct(){
+		$this->NetworkAPI = new Http();
+	}
+
+	public function Login($mail, $password){
+		$header1 = new Header()
+			.setHost('lobi.co')
+			.setConnection(true)
+			.setAccept('text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+			.setUserAgent('Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36')
+			.setAcceptLanguage('ja,en-US;q=0.8,en;q=0.6');
+
+		$source = $this->NetworkAPI->get('https://lobi.co/signin', $header1);
+		echo $source;
+	}
+}
